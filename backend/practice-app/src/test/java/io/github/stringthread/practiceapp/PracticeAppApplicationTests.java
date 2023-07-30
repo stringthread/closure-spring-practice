9065f5e87ad6587b4f3655bec4828cf29b4e9d42
stringthread <string.thread@gmail.com>
2023-07-30 23:11:44 +0900

    chore: install deps

---
+++ backend/practice-app/src/test/java/io/github/stringthread/practiceapp/PracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.stringthread.practiceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
